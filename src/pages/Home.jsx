913e8a5dc7d65aaf0ee4340f133a1bb4b8423517
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-06-12 19:10:39 +0800

    feat: added humidity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import TempIcon from '../assets/temp-icon.png';
+import HumIcon from '../assets/hum-icon.png';
 import { onValue, ref } from 'firebase/database';
 import { db } from '../firebase';
 import IOT from '../assets/iot-icon.png';
@@ -7,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 	const [temperature, setTemperature] = useState(null);
+	const [humidity, setHumidity] = useState(null);
 	const [loading, setLoading] = useState(true);
 
 	const navigate = useNavigate();
@@ -18,12 +20,22 @@ const Home = () => {
 
 	useEffect(() => {
 		const tempRef = ref(db, 'temp');
+		const humRef = ref(db, 'humidity');
 
 		onValue(tempRef, (snapshot) => {
 			const data = snapshot.val();
 			if (data !== null) {
 				console.log(data);
 				setTemperature(data);
+				setLoading(false);
+			}
+		});
+
+		onValue(humRef, (snapshot) => {
+			const data = snapshot.val();
+			if (data !== null) {
+				console.log(data);
+				setHumidity(data);
 				setLoading(false);
 			}
 		});
@@ -40,17 +52,34 @@ const Home = () => {
 						Group A LoRa Project
 					</h2>
 					<div className="text-center">
-						<div className="flex justify-center">
-							<img src={TempIcon} width={60} height={40} />
-							<p className="text-center text-slate-500 text-lg my-4">
-								Temperature
-							</p>
+						<div>
+							<div className="flex justify-center">
+								<img src={TempIcon} width={60} height={40} />
+								<p className="text-center text-slate-500 text-lg my-4">
+									Temperature
+								</p>
+							</div>
+							<div className="flex items-center justify-center bg-gray-600 py-2 rounded-md mt-4">
+								<h1 className="font-bold text-3xl text-white">
+									{loading ? 'Fetching...' : temperature}
+									<span className="text-white">&deg;C</span>
+								</h1>
+							</div>
 						</div>
-						<div className="flex items-center justify-center bg-gray-600 py-2 rounded-md mt-4">
-							<h1 className="font-bold text-3xl text-white">
-								{loading ? 'Fetching...' : temperature}
-								<span className="text-white">&deg;C</span>
-							</h1>
+
+						<div className="mt-6">
+							<div className="flex justify-center">
+								<img src={HumIcon} width={60} height={40} />
+								<p className="text-center text-slate-500 text-lg my-4">
+									Humidity
+								</p>
+							</div>
+							<div className="flex items-center justify-center bg-gray-600 py-2 rounded-md mt-4">
+								<h1 className="font-bold text-3xl text-white">
+									{loading ? 'Fetching...' : humidity}
+									<span className="text-white">%</span>
+								</h1>
+							</div>
 						</div>
 						<hr />
 						<button
